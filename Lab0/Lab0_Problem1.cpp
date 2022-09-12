c708d51c97748857b35912d55773474040cb5d41
Pratiksha Pai <paipratisha8@gmail.com>
2022-09-12 11:55:22 -0400

    updated Lab0 with comments

--- Lab0/Lab0_Problem1.cpp
+++ Lab0/Lab0_Problem1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * This function is a simplistic cpp function to test insertion stream operator and escape sequences
+*/
+
+
 int main () {
 
     cout << "My name is: Pratiksha Pai" << endl;
--- Lab0/Lab0_Problem2.cpp
+++ Lab0/Lab0_Problem2.cpp
@@ -1,15 +1,30 @@
+/*
+ * Author: Pratiksha Pundalik Pai
+ * Class: ECE6122
+ * Last Date Modified: 12th September 2022
+ * Description:
+ * This function implements One more one from projecteuler.net
+ * Details about the problem can be found at https://projecteuler.net/problem=672
+*/
+
 #include <iostream>
 using namespace std;
 
+/*
+ * Function implements one more one problem from projecteuler.net
+ * Inputs: Takes in "number" as input
+ * Outputs: None
+ * Prints: The function prints the number of 1's that are being added
+*/
 void oneMoreOne(int number){
 
     int count = 0;
-    while (number!=1){
+    while (number != 1){
         if (number%7 == 0){
-            number=number/7;
+            number = number/7;
         } else{
-            number+=1;
-            count+=1;
+            number += 1;
+            count += 1;
         }
     }
 
@@ -17,6 +32,11 @@ void oneMoreOne(int number){
     return;
 }
 
+
+/*
+ * This is main function or entrypoint for the code.
+ * This function checks for edge cases and runs oneMoreOne function.
+*/
 int main(){
 
     cout << "Please enter the starting number n (0 to stop): ";
